f8c7bd0790f896d602f41be121344d2ee39272c9
perlancar (@pc-office) <perlancar@gmail.com>
2016-10-12 10:07:49 +0700

    Initial commit

---
+++ lib/PerlIO/via/as_is.pm
@@ -0,0 +1,48 @@
+package PerlIO::via::as_is;
+
+# DATE
+# VERSION
+
+sub PUSHED {
+    my ($class, $mode, $fh) = @_;
+    bless [], $class;
+}
+
+sub FILL {
+    my ($self, $fh) = @_;
+    <$fh>;
+}
+
+sub WRITE {
+    my ($self, $buffer, $fh) = @_;
+    if (print $fh $buffer) { length($buffer) } else { -1 }
+}
+
+1;
+# ABSTRACT: PerlIO layer that passes everything as-is
+
+=for Pod::Coverage ^()$
+
+=head1 SYNOPSIS
+
+Reading:
+
+ use PerlIO::via::as_is;
+ open my($fh), "<:via(as_is)", "file";
+ print while <$fh>;
+
+Writing:
+
+ open my($fh), ">:via(as_is)", "file";
+ print $fh "one", "two\n";
+
+
+=head1 DESCRIPTION
+
+This PerlIO layer does nothing (reads/writes everything as-is). Created for
+testing purpose.
+
+
+=head1 SEE ALSO
+
+L<PerlIO::via>
